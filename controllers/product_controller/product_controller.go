cad2ccdbaa6c434d1fcb19f905b209f2a9a016ff
NofaFirdaus <nofafirdaus17@gmail.com>
2024-11-09 09:43:10 +0700

    .

--- controllers/product_controller/product_controller.go
+++ controllers/product_controller/product_controller.go
@@ -3,6 +3,9 @@ package productcontroller
 import (
 	"html/template"
 	"net/http"
+	"strconv"
+	"time"
+	"web/entities"
 	categorymodel "web/models/category_model"
 	productmodel "web/models/product_model"
 )
@@ -30,16 +33,29 @@ func Add(w http.ResponseWriter, r *http.Request) {
 		}
 		temp.Execute(w, data)
 	}
-	// if r.Method == "POST" {
-	// 	var category entities.Category
-	// 	category.Name = r.FormValue("name")
-	// 	timeString := time.Now().Format("2006-01-02 15:04:05")
-	// 	category.CreatedAt = []byte(timeString)
-	// 	category.UpdatedAt = []byte(timeString)
-	// 	if ok := categorymodel.Create(category); !ok {
-	// 		temp, _ := template.ParseFiles("views/category/add.html")
-	// 		temp.Execute(w, nil)
-	// 	}
-	// 	http.Redirect(w, r, "/categories", http.StatusSeeOther)
-	// }
+	if r.Method == "POST" {
+		var product entities.Product
+		product.Name = r.FormValue("name")
+		stock, err := strconv.Atoi(r.FormValue("stock"))
+		if err != nil {
+			panic(err)
+		}
+
+		product.Stock = uint(stock)
+		product.Description = r.FormValue("description")
+		categoryId, err := strconv.Atoi(r.FormValue("category_id"))
+		if err != nil {
+			panic(err)
+		}
+		product.CategoryId = uint(categoryId)
+		timeString := time.Now().Format("2006-01-02 15:04:05")
+		product.CreatedAt = []byte(timeString)
+		product.UpdatedAt = []byte(timeString)
+
+		if ok := productmodel.Create(product); !ok {
+			temp, _ := template.ParseFiles("views/product/add.html")
+			temp.Execute(w, nil)
+		}
+		http.Redirect(w, r, "/product", http.StatusSeeOther)
+	}
 }
--- models/product_model/productModel.go
+++ models/product_model/productModel.go
@@ -14,11 +14,24 @@ func GetAll() []entities.Product {
 	var products []entities.Product
 	for rows.Next() {
 		var product entities.Product
-		if err := rows.Scan(&product.Id, &product.Name, &product.Stock, &product.Description, &product.CreatedAt, &product.UpdatedAt); err != nil {
+		if err := rows.Scan(&product.Id, &product.Name, &product.CategoryId, &product.Stock, &product.Description, &product.CreatedAt, &product.UpdatedAt); err != nil {
 			panic(err)
 		}
 
 		products = append(products, product)
 	}
 	return products
+}
+
+func Create(product entities.Product) bool {
+	result, err := config.DB.Exec(`INSERT INTO products (name,category_id,stock,description,created_at,updated_at) 
+	VALUE (?,?,?,?,?,?)`, product.Name, product.CategoryId, product.Stock, product.Description, product.CreatedAt, product.UpdatedAt)
+	if err != nil {
+		panic(err)
+	}
+	last, err := result.LastInsertId()
+	if err != nil {
+		panic(err)
+	}
+	return last > 0
 }
